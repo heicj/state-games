8111100e3bb29406244500b927b34bf6d31de737
Charlie Heiner <heinercj@gmail.com>
2018-03-16 09:31:10 -0700

    error when adding to player inventory

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,12 +29,13 @@ class Main extends Component {
     const { room, player } = this.state;
     const index = room.items.indexOf(item);
     room.items.splice(index, 1);
-    player.inventory.push(item);
-
+    //const p = player.inventory.push(item);
+    const p1 = player.slice();
+    p1.push(item);
     this.setState({
       action: '',
       room,
-      player
+      p1
     });
   };
 
